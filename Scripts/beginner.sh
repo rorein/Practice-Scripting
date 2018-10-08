fe3600474500c45b640ea6b164df737c0b72c7b3
Rose <rose.reinlib@gmail.com>
2018-10-08 16:50:20 -0400

    Update Scripts/beginner.sh
    First practice script

---
+++ Scripts/beginner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+
+echo "The script starts now."
+USER="Rose"
+
+echo "Hi, $USER!"
+
+echo "I'm setting two variables now."
+COLOR="black"
+VALUE="9"
+echo
+
+echo "This is a string: $COLOR"
+echo "And this is a number: $VALUE"
+echo
+
+echo "I'm giving you back your prompt now."
+echo
